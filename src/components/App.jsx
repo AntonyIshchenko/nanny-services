679fd7882474651be9d0395cd3aabc527c3dd3c7
Antony Ishchenko <antony.ishchenko@gmail.com>
2024-08-10 00:44:52 +0300

    start-project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <div>HELLO</div>
+    </>
+  );
+}
+
+export default App;
